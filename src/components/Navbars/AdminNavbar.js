a22ce34de53c9166e28ec300ad6efacbe3c611e8
azeem2795 <muhammadazeem@falconit.com.pk>
2023-03-29 09:33:38 +0500

    Remove link from page name

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -178,12 +178,12 @@ const AdminNavbar = ({ brandText, getUser }) => {
     <>
       <Navbar className='navbar-top navbar-dark' expand='md' id='navbar-main'>
         <Container fluid>
-          <Link
+          <div
             className='h4 mb-0 text-white text-uppercase d-none d-lg-inline-block'
-            to='/admin/index'
+            // to='/admin/index'
           >
             {brandText}
-          </Link>
+          </div>
           <Nav className='align-items-center d-none d-md-flex' navbar>
             <UncontrolledDropdown nav>
               <DropdownToggle className='pr-0' nav>
