e2c3ef3ec1a05072cb2ba4545de46ad1ee6abdc1
Yousuf Kalim <yousufkalim@outlook.com>
2023-04-22 03:04:57 +0500

    Config update

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // let baseUrl = 'http://localhost:5000/api/v1';
 // let mediaUrl = 'http://localhost:5000/';
 let frontendUrl = 'http://localhost:3001';
-let baseUrl = 'https://vapeverification-api-dev.falconweb.app/api/v1';
-let mediaUrl = 'https://vapeverification-api-dev.falconweb.app/';
+let baseUrl = 'http://localhost:5000/api/v1';
+let mediaUrl = 'http://localhost:5000/';
 // let frontendUrl = 'https://vapeverification-dev.falconweb.app';
 
 if (window.location.host === 'vapeverification-admin-dev.falconweb.app') {
@@ -22,7 +22,7 @@ if (window.location.host === 'vapeverification-admin-dev.falconweb.app') {
 } else if (window.location.host === 'admin.getveriscan.com') {
   baseUrl = 'https://api.getveriscan.com/api/v1';
   mediaUrl = 'https://api.getveriscan.com/';
-  frontendUrl = 'https://client.getveriscan.com';
+  frontendUrl = 'https://app.getveriscan.com';
 }
 
 module.exports = { baseUrl, mediaUrl, frontendUrl };
